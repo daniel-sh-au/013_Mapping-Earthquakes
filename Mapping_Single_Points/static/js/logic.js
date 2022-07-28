4631b6a8bfdaf7b9209a3534d4de3858e684e90a
Daniel Au <daniel.au123@hotmail.com>
2022-07-28 14:42:27 -0400

    modifying logic.js in Mapping_Single_Points branch (13.4.1)

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -1,1 +1,35 @@
-// new logic.js
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+
+// Add a marker to the map for Los Angeles, California.
+let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// circle - radius in meters
+L.circle([34.0522, -118.2437], {
+    radius: 100, 
+    color: 'black',
+    fillColor: 'yellow'
+}).addTo(map);
+
+// circleMarker - radius in pixels, changes with zoom
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: '#ffffa1'
+}).addTo(map);
+
+
+
+// Modify previous code to use the Mapbox Styles API
+// We create the tile layer that will be the background of our map. /streets-v11
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
